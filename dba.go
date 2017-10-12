d0f5df6cb91e86fff92180241ffaddedeefac2d6
James Perry <tjamesperry@hotmail.com>
2017-10-12 15:24:01 -0700

    query builder created. WIP, am going to add to package in order to properly interface with database sql package and orthogonally server other packages.

--- dba.go
+++ dba.go
@@ -5,10 +5,11 @@ import (
 	"errors"
 	"fmt"
 	_ "github.com/minus5/gofreetds"
+	"strings"
 )
 
 var driver = "mssql"
-var momentConnStr = "Server=192.168.1.4:1433;Database=Moment-Db;User Id=Reader;Password=123"
+var momentConnStr = "Server=192.168.1.2:1433;Database=Moment-Db;User Id=Reader;Password=123"
 
 type Conn struct {
 	Db *sql.DB
@@ -68,4 +69,135 @@ func ValidateRowsAffected(res sql.Result, expected int) (err error) {
 	}
 
 	return
+}
+
+var (
+	ErrorEmptyStrings          = errors.New("Empty strings passed into Query.Select().")
+	ErrorSelectClauseEmpty     = errors.New("Query.selectClause is empty.")
+	ErrorFromClauseEmpty       = errors.New("Query.fromClause is empty.")
+	ErrorArgsParametersEmpty   = errors.New("No arguments passed into Query.Args().")
+	ErrorArgsParamsCntNotEqual = errors.New("Argument count does not equal parameter count.")
+	ErrorColumnDestCntNotEqual = errors.New("Column count does not equal destination count.")
+)
+
+type Query struct {
+	comment      string
+	selectClause []string
+	fromClause   string
+	joinClause   []string
+	whereClause  []string
+	args         []interface{}
+	destinations []*dest
+}
+
+func NewQuery(comment string) (q *Query) {
+	comment = "--" + comment
+	return &Query{
+		comment: comment,
+	}
+}
+
+func (q *Query) Selects(columns ...string) (err error) {
+	if len(columns) <= 0 {
+		return ErrorEmptyString
+	}
+
+	q.selectClause = append(q.selectClause, columns...)
+	return
+}
+
+func (q *Query) From(clause string) (err error) {
+	if clause == "" {
+		return ErrorEmptyString
+	}
+
+	q.fromClause = clause
+	return
+}
+
+func (q *Query) Join(clauses ...string) (err error) {
+	if len(clauses) <= 0 {
+		return ErrorEmptyString
+	}
+
+	q.joinClause = append(q.joinClause, clauses...)
+	return
+}
+
+func (q *Query) Where(clauses ...string) (err error) {
+	if len(clauses) <= 0 {
+		return ErrorEmptyString
+	}
+
+	q.whereClause = append(q.whereClause, clauses...)
+	return
+}
+
+func (q *Query) SetArgs(args ...interface{}) (err error) {
+	if len(args) <= 0 {
+		return ErrorArgsParametersEmpty
+	}
+	q.args = args
+	return
+}
+
+func (q *Query) Args() []interface{} {
+	return q.args
+}
+
+func (q *Query) SetDest(i interface{}, func()
+func (q *Query) Dest() []interface{} {
+	if util.IsEmpty(q.destinations) {
+		return ErrorDestinationsEmpty
+	}
+
+	dests := make([]interface{}, len(q.destinations))
+	for i, d := range q.destinations {
+		dests[i] = d.variable
+	}
+	return dests
+}
+
+type dest struct {
+	variable interface{}
+	handler  func()
+}
+
+func (q *Query) Build() (queryString string, err error) {
+	switch {
+	case len(q.selectClause) <= 0:
+		return ErrorSelectClauseEmpty
+	case q.fromClause == "":
+		return ErrorFromClauseEmpty
+	case q.argsParamsCntNotEqual():
+		return ErrorArgsParamsCntNotEqual
+	case q.columnDestCntNotEqual():
+		return ErrorColumnDestCntNotEqual
+	}
+
+	queryString = q.comment + `
+				  SELECT ` + strings.Join(q.selectClause, ", ") + `
+				  FROM ` + q.fromClause
+
+	if len(q.joinClause) > 0 {
+		queryString = queryString + "\nJOIN " + strings.Join(q.joinClause, "\nJOIN ")
+	}
+	if len(q.whereClause) > 0 {
+		queryString = queryString + "\nWHERE " + strings.Join(q.whereClause, "\n")
+	}
+	return
+}
+
+func (q *Query) argsParamsCntNotEqual() bool {
+	if strings.Count(strings.Join(q.whereClause, " "), "?") != len(q.args) {
+		return true
+	}
+	return false
+}
+
+func (q *Query) columnDestCntNotEqual() bool {
+	if len(q.selectClause) != len(q.dest) {
+		return true
+	}
+	return false
 }
